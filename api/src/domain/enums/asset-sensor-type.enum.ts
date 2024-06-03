3509a30be8af7daf950c70ceb47e513d85eef5d7
Gabriel Morais <gabriel8azevedo@gmail.com>
2024-06-03 09:20:59 -0300

    feat: add enums

---
+++ api/src/domain/enums/asset-sensor-type.enum.ts
@@ -0,0 +1,4 @@
+export enum AssetSensorTypeEnum {
+  energy = "energy",
+  vibration = "vibration",
+}
---
+++ api/src/domain/enums/asset-status.enum.ts
@@ -0,0 +1,4 @@
+export enum AssetStatusEnum {
+  critical = "critical",
+  operating = "operating",
+}
