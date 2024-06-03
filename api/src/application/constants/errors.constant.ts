a941895c3121e5fe39768b1edfcfc7b0815ea37b
Gabriel Morais <gabriel8azevedo@gmail.com>
2024-06-03 09:20:48 -0300

    feat: add constants

---
+++ api/src/application/constants/errors.constant.ts
@@ -0,0 +1,15 @@
+export const MISSING_REQUIRED_KEYS_ERROR: string = "Missing required keys: ";
+export const INVALID_ID_PARAM_ERROR: string = "Invalid id parameter";
+export const UNIT_NOT_FOUND_ERROR: string = "Unit not found";
+export const LOCATION_NOT_FOUND_ERROR: string = "Location not found";
+export const PARENT_LOCATION_NOT_FOUND_ERROR: string =
+  "Parent Location not found";
+export const LOCATION_COULD_NOT_BE_DELETED: string =
+  "Location could not be deleted";
+export const INVALID_SENSOR_TYPE: string =
+  "Invalid sensor type. valid types are: 'energy', 'vibration'";
+export const INVALID_STATUS =
+  "Invalid status. valid statuses are: 'operating', 'critical'";
+export const ASSET_NOT_FOUND_ERROR: string = "Asset not found";
+export const PARENT_ASSET_NOT_FOUND_ERROR: string = "Parent asset not found";
+export const ASSET_COULD_NOT_BE_DELETED: string = "Asset could not be deleted";
---
+++ api/src/application/constants/paths.constant.ts
@@ -0,0 +1,5 @@
+const UNIT_PATH = "/unit";
+const ASSET_PATH = "/asset";
+const LOCATION_PATH = "/location";
+
+export { UNIT_PATH, ASSET_PATH, LOCATION_PATH };
